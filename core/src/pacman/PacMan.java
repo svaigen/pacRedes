dad98e573f5e9f2c099bd50a46a98c1786fa17a8
Lailla <Lailla@LAILLA-PC>
2015-08-15 20:06:42 -0300

    pac anda

--- core/src/pacman/PacMan.java
+++ core/src/pacman/PacMan.java
@@ -20,6 +20,18 @@ import com.badlogic.gdx.math.Rectangle;
 import sun.security.x509.X500Name;
 
 public class PacMan extends ApplicationAdapter {
+    
+    float velocidade = 1.0f;
+
+    boolean andaDireita = false;
+    boolean andaEsquerda = false;
+    boolean andaCima = false;
+    boolean andaBaixo = false;
+
+    boolean proxAndaDireita = false;
+    boolean proxAndaEsquerda = false;
+    boolean proxAndaCima = false;
+    boolean proxAndaBaixo = false;
 
     SpriteBatch batch;
     Texture pacImg;
@@ -59,33 +71,10 @@ public class PacMan extends ApplicationAdapter {
 
         MapObjects objects = tiledMap.getLayers().get(2).getObjects();
 
-        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-            pac.translateX(-1.0f);
-            if (colidiu(objects)) {
-                pac.translateX(1.0f);
-            }
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-            pac.translateX(1.0f);
-            if (colidiu(objects)) {
-                pac.translateX(-1.0f);
-            }
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
-            pac.translateY(1.0f);
-            if (colidiu(objects)) {
-                pac.translateY(-1.0f);
-            }
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
-            pac.translateY(-1.0f);
-            if (colidiu(objects)) {
-                pac.translateY(1.0f);
-            }
-        }
-
-//        System.out.println(tiledMap.getLayers().get(1).getObjects().get(0));
-        //TiledMapTileLayer collisionObjectLayer = (TiledMapTileLayer) tiledMap.getLayers().get(2);
+        ativaProxPasso();
+        tentaProxPasso(objects);
+        andar(objects);
+        
         batch.begin();
         pac.draw(batch);
         batch.end();
@@ -98,13 +87,117 @@ public class PacMan extends ApplicationAdapter {
         pacImg.dispose();
     }
 
-    private boolean colidiu(MapObjects objects) {
+    private boolean colidiu(MapObjects objects, int direcao) {
+        float x = pac.getX();
+        float y = pac.getY();
+        
+        switch(direcao){
+            case 0: //esquerda
+                x -= velocidade;
+                break;
+            case 1: //direita
+                x += velocidade;
+                break;
+            case 2: //baixo
+                y -= velocidade;
+                break;
+            case 3://cima
+                y += velocidade;
+                break;
+        }
+        Rectangle r = new Rectangle(x, y, pac.getWidth(), pac.getHeight());
         for (RectangleMapObject rectangleObject : objects.getByType(RectangleMapObject.class)) {
-            Rectangle rectangle = rectangleObject.getRectangle();
-            if (Intersector.overlaps(rectangle, pac.getBoundingRectangle())) {
+            Rectangle rectangle = rectangleObject.getRectangle();           
+            if (Intersector.overlaps(rectangle, r)) {
                 return true;
             }
         }
         return false;
+    }
+
+    private void ativaProxPasso() {
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            proxAndaEsquerda = true;
+            proxAndaDireita = false;
+            proxAndaCima = false;
+            proxAndaBaixo = false;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            proxAndaEsquerda = false;
+            proxAndaDireita = true;
+            proxAndaCima = false;
+            proxAndaBaixo = false;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            proxAndaEsquerda = false;
+            proxAndaDireita = false;
+            proxAndaCima = true;
+            proxAndaBaixo = false;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            proxAndaEsquerda = false;
+            proxAndaDireita = false;
+            proxAndaCima = false;
+            proxAndaBaixo = true;
+        }
+    }
+
+    private void tentaProxPasso(MapObjects objects) {
+        if (proxAndaEsquerda) {
+            if (!colidiu(objects,0)) {
+                andaEsquerda = true;
+                andaDireita = false;
+                andaCima = false;
+                andaBaixo = false;
+            }
+        }
+        if (proxAndaDireita) {
+            if (!colidiu(objects,1)) {
+                andaEsquerda = false;
+                andaDireita = true;
+                andaCima = false;
+                andaBaixo = false;
+            }
+        }
+        if (proxAndaCima) {
+            if (!colidiu(objects,3)) {
+                andaEsquerda = false;
+                andaDireita = false;
+                andaCima = true;
+                andaBaixo = false;
+            }
+        }
+        if (proxAndaBaixo) {
+            if (!colidiu(objects,2)) {
+                andaEsquerda = false;
+                andaDireita = false;
+                andaCima = false;
+                andaBaixo = true;
+            }
+        }
+    }
+
+    private void andar(MapObjects objects) {
+        if (andaEsquerda) {
+            if (!colidiu(objects,0)) {
+                pac.translateX(-velocidade);
+            }
+        }
+        if (andaDireita) {
+            if (!colidiu(objects,1)) {
+                pac.translateX(velocidade);
+            }
+        }
+        if (andaCima) {
+            if (!colidiu(objects,3)) {
+                pac.translateY(velocidade);
+            }
+        }
+        if (andaBaixo) {
+            if (!colidiu(objects,2)) {
+                pac.translateY(-velocidade);
+            }
+        }
+
     }
 }
